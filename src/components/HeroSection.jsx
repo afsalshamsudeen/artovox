e32cd5fa423893eabeb2cb7bda979239dfe55864
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2025-03-01 22:16:34 +0530

    Hero Section Completed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,28 +1,40 @@
 
 import styled from 'styled-components';
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import Sample_img1 from "../assets/tomato.jpg";
+import Sample_img2 from "../assets/panda.jpg";
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    p{
+      font-weight: 200;
+      font-size: 12px;
+    }
+`;
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding-top: 10px;
+    padding-top: 8px;
     h1{
         text-align: center;
         font-size: 50px;
         font-weight: 400;
     }
     p{
-        width: 650px;
+        width: 700px;
         word-break: break-word;
         text-align: center;
         font-weight: 300;
         font-size: 16px;
         white-space: normal; 
         overflow: visible; 
-        text-overflow: clip; 
+        text-overflow: clip;
+        letter-spacing: 0.5px;
     }
     span{
         color: #7FFFD4;
@@ -39,7 +51,8 @@ const Wrapper = styled.div`
         align-items: center;
         justify-content: center;
         gap: 5px;
-        padding-left: 10px;
+      
+        padding: 7px 14px;
         cursor: pointer;
 
         &:hover {
@@ -61,6 +74,28 @@ const Wrapper = styled.div`
     }
 `;
 
+const SampleSection = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 40px;
+    gap: 16px;
+    overflow: visible;
+
+`;
+const RandomImages = styled.img`
+    display: flex;
+    flex-direction: row;
+    border-radius: 10px;
+    display: block;
+    transition: transform 0.3s ease-in-out !important;
+    &:hover{
+      transform: scale(1.05);
+    }
+    cursor: pointer;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+`;
+
 const HeroSection = () => {
   return (
     <Container>
@@ -70,7 +105,15 @@ const HeroSection = () => {
         <div>
             <p>Try Artoxox</p><ArrowOutwardIcon/>
         </div>
+
+        
       </Wrapper>
+      <SampleSection>
+          {Array(5).fill('').map((item, index)=>(
+            <RandomImages src={index % 2 ===0 ? Sample_img1 : Sample_img2} key={index} width={70}/>
+          ))}
+        </SampleSection>
+          <p>Generated images from Artovox</p>
     </Container>
   )
 }
